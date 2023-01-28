3e6bfbaa63a32db135cde620aa6d289fc142c6ff
Johnny B <johnny.bee@mywebsite.internet>
2023-01-29 00:37:59 +0100

    add json data

--- src/app.js
+++ src/app.js
@@ -8,9 +8,30 @@ const express = require('express');
 const app=express();
 const PORT = process.env.PORT;
 
+const customers = [
+    {
+        "name": "Johnny B",
+        "industry": "music"
+    },
+    {
+        "name": "Joe",
+        "industry": "networking"
+    },
+    {
+        "name": "Sal",
+        "industry": "sports"
+ 
+    }
+];
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world! </h1>');
+    res.send("Welcome!");
 });
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
+});
+
 
 app.post('/', (req, res) => {
     res.send('This is a post request!');
